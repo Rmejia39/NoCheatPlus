ab80a072cffe45a607896c2e3acd44b5a9f35021
rigoberto <88948582+Rmejia39@users.noreply.github.com>
2023-12-20 09:35:18 -0600

    update file

---
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/command/admin/InspectCommand.java
@@ -0,0 +1,127 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.neatmonster.nocheatplus.command.admin;
+
+import java.text.DecimalFormat;
+import java.util.Collection;
+import java.util.List;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+
+import fr.neatmonster.nocheatplus.command.BaseCommand;
+import fr.neatmonster.nocheatplus.compat.BridgeHealth;
+import fr.neatmonster.nocheatplus.permissions.Permissions;
+import fr.neatmonster.nocheatplus.players.DataManager;
+
+public class InspectCommand extends BaseCommand {
+    private static final DecimalFormat f1 = new DecimalFormat("#.#");
+
+    public InspectCommand(JavaPlugin plugin) {
+        super(plugin, "inspect", Permissions.COMMAND_INSPECT);
+    }
+
+    /* (non-Javadoc)
+     * @see fr.neatmonster.nocheatplus.command.AbstractCommand#onCommand(org.bukkit.command.CommandSender, org.bukkit.command.Command, java.lang.String, java.lang.String[])
+     */
+    @Override
+    public boolean onCommand(CommandSender sender, Command command,
+            String alias, String[] args) {
+        if (args.length == 1) {
+            if (sender instanceof Player) {
+                args = new String[]{args[0], sender.getName()};
+            } else {
+                sender.sendMessage(TAG + "Please specify a player to inspect.");
+                return true;
+            }
+        }
+        final ChatColor c1;
+        if (sender instanceof Player) {
+            c1 = ChatColor.GRAY;
+        } else {
+            c1 = null;
+        }
+        for (int i = 1; i < args.length; i++) {
+            final Player player = DataManager.getPlayer(args[i].trim().toLowerCase());
+            if (player == null) {
+                sender.sendMessage("(Not online: " + args[i] + ")");
+            } else {
+                sender.sendMessage(getInspectMessage(player, c1));
+            }
+        }
+        return true;
+    }
+
+    public static String getInspectMessage(final Player player, final ChatColor contentColor) {
+        final String c1 = contentColor == null ? "" : contentColor.toString();
+        final StringBuilder builder = new StringBuilder(256);
+        builder.append(player.getName() + c1);
+        builder.append(" (" + (player.isOnline() ? "online" : "offline") + "," + (player.isOp() ? "!OP!," : "") + player.getGameMode() + (player.isDead() ? ",dead" : "") + (player.isValid() ? "" : ",invalid") + (player.isInsideVehicle() ? (",vehicle=" + player.getVehicle().getType() + "@" + locString(player.getVehicle().getLocation())) : "")+ "):");
+        // TODO: isValid, isDead,  isInsideVehicle ...
+        // Health.
+        builder.append(" health=" + f1.format(BridgeHealth.getHealth(player)) + "/" + f1.format(BridgeHealth.getMaxHealth(player)));
+        // Food.
+        builder.append(" food=" + player.getFoodLevel());
+        // Exp.
+        if (player.getExp() > 0f) {
+            builder.append(" explvl=" + f1.format(player.getExpToLevel()) + "(exp=" + f1.format(player.getExp()) + ")");
+        }
+        // Fly settings.
+        if (player.isFlying()) {
+            builder.append(" flying");
+        }
+        if (player.getAllowFlight()) {
+            builder.append(" allowflight");
+        }
+        // Speed settings.
+        builder.append(" flyspeed=" + player.getFlySpeed());
+        builder.append(" walkspeed=" + player.getWalkSpeed());
+        // Potion effects.
+        final Collection<PotionEffect> effects = player.getActivePotionEffects();
+        if (!effects.isEmpty()) {
+            builder.append(" effects=");
+            for (final PotionEffect effect : effects) {
+                builder.append(effect.getType() + "@" + effect.getAmplifier() +",");
+            }
+        }
+        // TODO: is..sneaking,sprinting,blocking,
+        // Finally the block location.
+        final Location loc = player.getLocation();
+        builder.append(" pos=" + locString(loc));
+        return builder.toString();
+    }
+
+    private static final String locString(Location loc) {
+        return loc.getWorld().getName() + "/" + loc.getBlockX() + "," + loc.getBlockY() + "," + loc.getBlockZ();
+    }
+
+    /* (non-Javadoc)
+     * @see fr.neatmonster.nocheatplus.command.AbstractCommand#onTabComplete(org.bukkit.command.CommandSender, org.bukkit.command.Command, java.lang.String, java.lang.String[])
+     */
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command,
+            String alias, String[] args) {
+        // Complete players.
+        return null;
+    }
+
+
+
+}
