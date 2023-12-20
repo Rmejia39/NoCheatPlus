45f300a680e3c447562f594526068a2757388795
rigoberto <88948582+Rmejia39@users.noreply.github.com>
2023-12-20 09:35:03 -0600

    update file

---
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/inventory/InventoryData.java
@@ -0,0 +1,58 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.neatmonster.nocheatplus.checks.inventory;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.InventoryView;
+
+import fr.neatmonster.nocheatplus.checks.access.ACheckData;
+import fr.neatmonster.nocheatplus.utilities.ds.count.ActionFrequency;
+
+/**
+ * Player specific dataFactory for the inventory checks.
+ */
+public class InventoryData extends ACheckData {
+
+    // Violation levels.
+    public double   dropVL;
+    public double   fastClickVL;
+    public double   instantBowVL;
+    public double   instantEatVL;
+
+    // General.
+    public long     lastClickTime = 0;
+
+    // Data of the drop check.
+    public int      dropCount;
+    public long     dropLastTime;
+
+    // Data of the fast click check.
+    //    public boolean  fastClickLastCancelled;
+    public final ActionFrequency fastClickFreq = new ActionFrequency(5, 200L);
+    public Material fastClickLastClicked = null;
+    public int fastClickLastSlot = InventoryView.OUTSIDE;
+    public Material fastClickLastCursor = null;
+    public int fastClickLastCursorAmount = 0;
+
+    // Data of the instant bow check.
+    /** Last time right click interact on bow. A value of 0 means 'invalid'.*/
+    public long     instantBowInteract = 0;
+    public long     instantBowShoot;
+
+    // Data of the instant eat check.
+    public Material instantEatFood;
+    public long     instantEatInteract;
+
+}
